6adcfd2feffd973c235d38e0b7892fc13cd0ed27
jinx420 <76714483+jinx420@users.noreply.github.com>
2023-05-19 23:52:01 +0200

    added some text

--- compile.bat
+++ compile.bat
@@ -1,3 +1,18 @@
 @echo off
+title Compiler v0.1.0
+echo +++++++++++++++++++
+echo + Compiler v0.1.0 +
+echo +   By: jinx420   +
+echo +  Compiling...   +
+echo +++++++++++++++++++
+echo.
+echo If you encounter any errors, please report them at https://github.com/jinx420/isThisWebsiteOnline/issues 
+echo or contact me on Discord: majoad#0001
+echo.
+
 pyinstaller -F -w --icon=iwoSource\favicon.ico -n isThisWebsiteOnline isThisWebsiteOnline.py
 python .\\move.py
+
+echo.
+echo Compiling finished! Press any key to exit...
+pause > nul
